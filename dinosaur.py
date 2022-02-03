a86be7c9ecff27136f5b671e76230a81a1c157be
unknown <kenblackuoo@gmail.com>
2022-02-03 14:28:50 -0500

    Created files for each Class that will be required for this project

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+class Dinosaur:
+     
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+
+    def attack(self, robot):
+        pass
+
