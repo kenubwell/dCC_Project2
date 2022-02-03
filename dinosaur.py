b09adc3df6726754a192f72b4b3ccad8f261cd8a
unknown <kenblackuoo@gmail.com>
2022-02-03 17:14:44 -0500

    Defined the attack methods in both the Robot and Dinossaur classes

--- dinosaur.py
+++ dinosaur.py
@@ -2,8 +2,8 @@ class Dinosaur:
      
     def __init__(self, name, attack_power):
         self.name = name
+        self.health = 100
         self.attack_power = attack_power
 
-    def attack(self, robot):
-        pass
-
+    def dino_attack(self, targeted_robot):
+        targeted_robot.health -= self.attack_power #tentative need to evaluate
--- robot.py
+++ robot.py
@@ -2,11 +2,10 @@ from weapon import Weapon
 
 class Robot:
      
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, robot_name):
+        self.robot_name = robot_name
         self.health = 100
-        self.weapon = Weapon()
+        self.weapon = Weapon("Sword", 50)  
 
-    def attack(self, dinosaur):
-        pass
-
+    def robot_attack(self, targeted_dinosaur): 
+        targeted_dinosaur.health -= self.weapon.attack_power #tentative need to evaluate
